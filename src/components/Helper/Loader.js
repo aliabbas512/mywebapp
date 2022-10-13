f1b150448fd029283607c6bfc1e8b812af5077d8
Syed Ali Abbas <aliabbasshah72@gmail.com>
2022-10-13 19:47:39 +0500

    mywebapp4

--- src/components/Helper/Loader.js
+++ src/components/Helper/Loader.js
@@ -3,8 +3,6 @@ import React from 'react';
 function Loader() {
     return (
         <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xlink="http://www.w3.org/1999/xlink"
             style={{
                 margin: 'auto',
                 display: 'block',
